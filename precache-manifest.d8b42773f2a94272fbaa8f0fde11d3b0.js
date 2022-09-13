58417489b0a7a1c74979a009d0cdbb0b8bcedf7e
guilhermerocha1 <guilhermerochasilva318@outlook.com>
2022-09-13 12:22:22 -0300

    Updates

---
+++ precache-manifest.d8b42773f2a94272fbaa8f0fde11d3b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4d6ec684359d7d1e51334c2b9074ae8",
+    "url": "/project-wallet-redux/index.html"
+  },
+  {
+    "revision": "cdd043e8a5323b86bfa2",
+    "url": "/project-wallet-redux/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ef3de18a377bbf2adaf9",
+    "url": "/project-wallet-redux/static/js/2.5cf46cfb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project-wallet-redux/static/js/2.5cf46cfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdd043e8a5323b86bfa2",
+    "url": "/project-wallet-redux/static/js/main.63f4ec59.chunk.js"
+  },
+  {
+    "revision": "24e67654df6b6311ab99",
+    "url": "/project-wallet-redux/static/js/runtime-main.b56ae5fd.js"
+  }
+]);
